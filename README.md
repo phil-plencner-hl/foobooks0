8a3fefcd079e6184b5e81586014800a7cafb6cdb
Phil Plencner <philip_plencner@harvard.edu>
2019-02-18 11:51:07 -0500

    Updated markup and styles. Added README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# foobooks0
+# Foobooks0
++ By: Phil Plencner
++ Production URL: <http://foobooks0.plencnerlabs.me>
+
+## Outside resources
++ Styling via [Bootstrap CDN](https://www.bootstrapcdn.com)
++ Book cover images from [Barnes and Noble](https://www.barnesandnoble.com)
+
+## 3 Unique inputs
+TBD
+
+## Class
+TBD
+
+## Code style divergences
+None
+
+## Notes for instructor
+None
--- index.php
+++ index.php
@@ -10,17 +10,27 @@ require 'logic.php';
     <title>Foobooks0</title>
     <meta charset='utf-8'>
 
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+
+    <link href='/styles/app.css' rel='stylesheet'>
+
 </head>
 <body>
 
 <h1>Foobooks0</h1>
 
-<?php foreach ($books as $title => $book): ?>
-    <div class='book'>
-        <?= $title ?> by <?= $book['author'] ?>
-        <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
-    </div>
-<?php endforeach ?>
+<ul class='books'>
+    <?php foreach ($books as $title => $book): ?>
+        <li class='book'>
+            <div class='title'><?= $title ?></div>
+            <div class='author'> by <?= $book['author'] ?></div>
+            <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
+        </li>
+    <?php endforeach ?>
+</ul>
 
 </body>
 </html>
